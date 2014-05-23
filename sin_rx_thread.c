f620eb8f5b5b68f210b8a9ce899b1bf55a27e602
User Ssp-root <ssp-root@netmap_test.sippysoft.com>
2014-05-23 16:18:34 -0700

    Make sure slot is empty when returning packet to the pkt_zone.

--- sin_rx_thread.c
+++ sin_rx_thread.c
@@ -78,6 +78,9 @@ static inline void
 return_pkt(struct sin_pkt *pkt, struct sin_pkt_zone *pzone)
 {
 
+#ifdef SIN_DEBUG
+    assert(pzone->first[pkt->zone_idx] == NULL);
+#endif
     pzone->first[pkt->zone_idx] = pkt;
 }
 
