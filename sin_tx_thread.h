7d77a35ce57c6107e0760bf4add6949f7b911fde
User Ssp-root <ssp-root@netmap_test.sippysoft.com>
2014-05-24 10:28:20 -0700

    Add sin_tx_thread_get_out_queue() prototype.

--- sin_tx_thread.h
+++ sin_tx_thread.h
@@ -1,2 +1,4 @@
 struct sin_tx_thread *sin_tx_thread_ctor(struct sin_stance *sip, int *sin_err);
 void sin_tx_thread_dtor(struct sin_tx_thread *sttp);
+struct sin_wi_queue *sin_tx_thread_get_out_queue(struct sin_tx_thread *sttp);
+
