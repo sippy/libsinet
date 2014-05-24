f687948da13b5266240124fec45aa9352e7f7214
User Ssp-root <ssp-root@netmap_test.sippysoft.com>
2014-05-24 11:05:13 -0700

    Remove defunct forward declaration and convert lengths and indexes into unsigned int from signed.

--- sin_pkt.h
+++ sin_pkt.h
@@ -1,14 +1,16 @@
-struct netmap_slot;
+struct sin_pkt_zone;
 
 struct sin_pkt
 {
     struct sin_type_linkable t;
-    int zone_idx;
+    struct sin_pkt_zone *my_zone;
+    unsigned int zone_idx;
     struct timeval ts;
     char *buf;
-    int len;
+    unsigned int len;
 };
 
-struct sin_pkt *sin_pkt_ctor(int zone_idx, int *sin_err);
+struct sin_pkt *sin_pkt_ctor(struct sin_pkt_zone *my_zone, int zone_idx,
+  int *sin_err);
 void sin_pkt_dtor(struct sin_pkt *pkt);
 
