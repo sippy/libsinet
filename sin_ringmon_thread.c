90f0da6ba6a23ab14c2a2e054f2b4444fe28022a
Maksym Sobolyev <sobomax@sippysoft.com>
2014-06-16 09:46:40 -0700

    Add sin_ringmon_register() to hook up ringmon with the interested rx threads.

--- sin_ringmon_thread.c
+++ sin_ringmon_thread.c
@@ -79,11 +79,37 @@ sin_ringmon_thread_ctor(const char *tname, int netmap_fd,
     return (srmtp);
 }
 
+int
+sin_ringmon_register(struct sin_ringmon_thread *srmtp, struct netmap_ring *rx_ring,
+  void (*client_wakeup)(void *), void *wakeup_arg, int *e)
+{
+    struct ringmon_client *rmcp;
+
+    rmcp = malloc(sizeof(struct ringmon_client));
+    if (rmcp == NULL) {
+        _SET_ERR(e, ENOMEM);
+        return (-1);
+    }
+    memset(rmcp, '\0', sizeof(struct ringmon_client));
+    SIN_TYPE_SET(rmcp, _SIN_TYPE_ITERABLE);
+    rmcp->rx_ring = rx_ring;
+    rmcp->client_wakeup = client_wakeup;
+    rmcp->wakeup_arg = wakeup_arg;
+    SIN_TYPE_LINK(rmcp, srmtp->first);
+    srmtp->first = rmcp;
+    return (0);
+}
+
 void
 sin_ringmon_thread_dtor(struct sin_ringmon_thread *srmtp)
 {
+    struct ringmon_client *rmcp, *rmcp_next;
 
     SIN_TYPE_ASSERT(srmtp, _SIN_TYPE_WRK_THREAD);
     sin_wrk_thread_dtor(&srmtp->t);
+    for (rmcp = srmtp->first; rmcp != NULL; rmcp = rmcp_next) {
+        rmcp_next = SIN_ITER_NEXT(rmcp);
+        free(rmcp);
+    }
     free(srmtp);
 }
--- sin_ringmon_thread.h
+++ sin_ringmon_thread.h
@@ -5,3 +5,5 @@ struct sin_pkt_zone;
 struct sin_ringmon_thread *sin_ringmon_thread_ctor(const char *tnane,
   int netmap_fd, int *sin_err);
 void sin_ringmon_thread_dtor(struct sin_ringmon_thread *srmtp);
+int sin_ringmon_register(struct sin_ringmon_thread *srmtp, struct netmap_ring *rx_ring,
+  void (*client_wakeup)(void *), void *wakeup_arg, int *e);
